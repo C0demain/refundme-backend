a32ee063d96be0076d517ce74ba47544e4a48eef
Mateus Madeira <mafemad2005@gmail.com>
2025-03-13 11:33:20 -0300

    feat: banco de dados na nuvem 🍃
    crud basico user

---
+++ src/user/user.module.ts
@@ -0,0 +1,14 @@
+import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+import { UserService } from "./user.service";
+import { UserController } from "./user.controller";
+import { userSchema } from "./user.schema";
+
+@Module({
+    imports:[MongooseModule.forFeature([{name: 'User', schema: userSchema}])],
+    providers: [UserService],
+    controllers: [UserController],
+})
+export class UserModule{
+
+}
---
+++ src/user/user.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
+import { User } from "./user.schema";
+import { CreateUserDto } from "./dtos/createUser.dto";
+import { UpdateUserDto } from "./dtos/updateUser.dto";
+
+
+@Injectable()
+export class UserService{
+    constructor(@InjectModel(User.name) private userModel: Model<User>){}
+    
+    async createUser(createUserDto: CreateUserDto){
+        const newUser = new this.userModel(createUserDto);
+
+        return newUser.save()
+    }
+
+    async getUsers(){
+        return this.userModel.find().exec();
+    }
+    
+    async getUserById(id: string){
+        return this.userModel.findById(id).exec();
+    }
+    
+    async updateUser(id: string, updateUser: UpdateUserDto){
+        return this.userModel.findByIdAndUpdate(id, updateUser,{new: true}).exec();
+    }
+
+    deleteUser(id: string) {
+        return this.userModel.findByIdAndDelete(id).exec();
+    }
+}
