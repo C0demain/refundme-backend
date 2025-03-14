051d02f9aabe4801c471673998647652bcef0e45
Mateus Madeira <mafemad2005@gmail.com>
2025-03-14 08:32:25 -0300

    bakcend e banco de dados docker 🐳
    para caso queiram rodar local pelo, importante mudar o app.module para corresponder a url do banco que for usar

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), MongooseModule.forRoot(process.env.MONGODB_URI || ""), UserModule],
+  imports: [ConfigModule.forRoot(), MongooseModule.forRoot(process.env.MONGODB_URI_DOCKER || ""), UserModule],
   controllers: [],
   providers: [],
 })
