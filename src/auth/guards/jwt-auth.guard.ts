a13027ff740194941f135cabb89c92d4df6b3a29
Mateus Madeira <mafemad2005@gmail.com>
2025-03-25 11:33:24 -0300

    refactor: agora somente web vai usar token
    mobile nao vai mais precisar passar o token para acessar as rotas protegidas

--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -1,5 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+    canActivate(context: ExecutionContext): boolean {
+        const request = context.switchToHttp().getRequest();
+        const clientType = request.headers['client-type'];
+    
+        // Permitir requisições sem token se vier do mobile
+        if (clientType === 'mobile') {
+          return true;
+        }
+    
+        // Caso contrário, usa a autenticação padrão do Passport JWT
+        return super.canActivate(context) as boolean;
+      }
+    
+      handleRequest(err, user, info) {
+        if (err || !user) {
+          throw err || new UnauthorizedException('Token inválido ou ausente');
+        }
+        return user;
+      }
+}
